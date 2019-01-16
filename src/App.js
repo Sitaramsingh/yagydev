0fbf6636d621f85e1ce50dc9e03025f670d01cb4
Sita Ram <sitaramcratis@gmail.com>
2019-01-16 19:58:50 +0530

    Added app file by ram

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import TableData from './components/tablesData';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './components/Home';
-import ProductList from './components/USAAComponent/ProductList';
-import CreateHelp from './components/CreateHelp/CreateHelp';
-import DragDrop from './components/DragDropList/DragDropList'
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <Router>
-          <Switch>
-            {/* <Route exact path='/' component={Home}/> */}
-            <Route exact path='/dragDrop' component={DragDrop}/>
-            <Route exact path='/createHelp' component={CreateHelp}/>
-            <Route exact path ='/product' component={ProductList}></Route>
-            <Route exact path='/' component={TableData}/>
-          </Switch>
-        </Router>       
+        <h1>Welvcome to home</h1>     
       </div>
     );
   }
